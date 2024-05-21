cd7af40f6d3c284e6cda7a35367fa3131888f81a
MoyaJoseDev <josetomasmoya.dev@gmail.com>
2024-05-21 01:25:43 -0300

    loader y opt img

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const backgrounds = ['background1.jpg', 'background2.jpg', 'background3.jpg', 'background4.jpg'];
+window.addEventListener('load', function() {
+  var loadingScreen = document.getElementById('loadingScreen');
+  loadingScreen.style.display = 'none';
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const backgrounds = [
+    "background1.jpg",
+    "background2.jpg",
+    "background3.jpg",
+    "background4.jpg",
+  ];
   const intervalTime = 5000; // 5 segundos en milisegundos
-  
+
   let currentIndex = 0;
-  const mainElement = document.getElementById('principal');
-  
+  const mainElement = document.getElementById("principal");
+
   // Pre-cargar imágenes en un arreglo
   const imagePreload = [];
-  backgrounds.forEach(function(background) {
-      const img = new Image();
-      img.src = `images/${background}`;
-      imagePreload.push(img);
+  backgrounds.forEach(function (background) {
+    const img = new Image();
+    img.src = `images/${background}`;
+    imagePreload.push(img);
   });
-  
+
   function changeBackground() {
-      mainElement.style.backgroundImage = `url("images/${backgrounds[currentIndex]}")`;
-      currentIndex = (currentIndex + 1) % backgrounds.length;
+    mainElement.style.backgroundImage = `url("images/${backgrounds[currentIndex]}")`;
+    currentIndex = (currentIndex + 1) % backgrounds.length;
   }
-  
+
   // Cambia el fondo cada 15 segundos
   setInterval(changeBackground, intervalTime);
-  
+
   // Cambia el fondo al cargar la página
   changeBackground();
 });
